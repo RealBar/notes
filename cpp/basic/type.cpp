ad07c28da81c4eca6c1a725cd1b9b0d9fca3a89a
wangyalou <1374178468@qq.com>
2023-12-08 08:42:12 +0800

    feat: 增加了对const的学习笔记

--- cpp/basic/type.cpp
+++ cpp/basic/type.cpp
@@ -1,6 +1,42 @@
 #include<iostream>
+#include<string>
+
+class User{
+private:
+    std::string _name;
+    int _age;
+public:
+    User() = default;
+    ~User() = default;
+    User(std::string name, int age):_name(name),_age(age){};
+    void SetName(std::string name){
+        _name = name;
+    }
+        std::string GetName() const {
+        return _name;
+    }
+};
+
 
 int main(){
-    std::cout<<"hello"<<std::endl;
+    int a = 1;
+    const int &b = a;
+    a = 2;
+    std::cout << b << std::endl;
+    
+
+    User u{"章三",12};
+
+    int c = 1;
+    int &d = c;
+     int const &e = 2;
+
     return 0;
+}
+
+void ProcessUser(const User &u){
+    // error: the object has type qualifiers that are not compatible with the member function "User::SetName"C/C++(1086)
+    // type.cpp(31, 5): object type is: const User
+    // u.SetName("李四");
+    std::cout << u.GetName();
 }
